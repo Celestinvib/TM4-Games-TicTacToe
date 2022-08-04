89a51cd51a27806e63c40a5b3a1ec182ba4cd8fc
Celestinvib <celestinvib@gmail.com>
2022-08-04 17:13:43 +0200

    Controller v0.8 (security improved/some comments added)

--- TM4-Games-TicTacToe/src/main/java/Controller/ControllerGame.java
+++ TM4-Games-TicTacToe/src/main/java/Controller/ControllerGame.java
@@ -35,36 +35,48 @@ public class ControllerGame {
 		buttons[2] = row3;
 		
 		app.getBtnNewGame().addActionListener(new ActionListener() {
-			// Starts a new Game	
 			public void actionPerformed(ActionEvent e) {
-				newGame();
+				newGame();	// Starts a new Game	
 			}
 		});
 		
 		buttonBoardActions();
 	}
 	
+	/**
+	 * Method that starts a new Game 
+	 * 
+	 */
 	public void newGame() {
+		int newMatch = 0;
 		
-		
-		board.prepareEmptyBoard();
-		for (int i = 0; i < buttons.length; i++) {
-			for (int j = 0; j < buttons.length; j++) {
-				buttons[i][j].setText("");
-			}
+		if(gameStarted) { //Check if a game has started , if it has ask the user if he really want to start a new game 
+		    newMatch = JOptionPane.showConfirmDialog(null, "Estas seguro que quieres empezar una nueva partida?");
 		}
 		
-		if (validarDatos(app.getTextFieldPlayer1Name(), app.getTextFieldPlayer2Name())) {
-			ply1 = new Player("X", app.getTextFieldPlayer1Name().getText().toString(), "Humano");
-			ply2 =  new Player("O", app.getTextFieldPlayer2Name().getText().toString(), "CPU");
-			gameStarted = true;
-			app.getLblMovements().setText("");
-			turn = "X";
+		if(newMatch == 0 ) { //If a game has not started or a user wants to restart a game
+			
+			//Clear the board object and the buttons of GraphicApp
+			board.prepareEmptyBoard();
+			for (int i = 0; i < buttons.length; i++) {
+				for (int j = 0; j < buttons.length; j++) {
+					buttons[i][j].setText("");
+				}
+			}
+			
+			if (checkNameFields(app.getTextFieldPlayer1Name(), app.getTextFieldPlayer2Name())) {
+				ply1 = new Player("X", app.getTextFieldPlayer1Name().getText().toString(), "Humano"); //<--------- Set the type of that the user have entered in the radiobutton!!!
+				ply2 =  new Player("O", app.getTextFieldPlayer2Name().getText().toString(), "CPU");
+				gameStarted = true;
+				app.getLblMovements().setText("");
+				turn = "X";
+			}
 		}
 		
 	}
 	
-	public boolean validarDatos(JTextField textFieldPlayer1Name, JTextField textFieldPlayer2Name) {
+	
+	public boolean checkNameFields(JTextField textFieldPlayer1Name, JTextField textFieldPlayer2Name) {
 		
 		if(textFieldPlayer1Name.getText().equals("") || textFieldPlayer2Name.getText().equals("")) {
 			JOptionPane.showMessageDialog(null, "No pueden haber campos vacios", null, JOptionPane.ERROR_MESSAGE);
@@ -73,6 +85,10 @@ public class ControllerGame {
 		return true;
 }
 	
+	/**
+	 * Method that adds listeners to the buttons in the graphic part and the actions that they do
+	 * 
+	 */
 	public void buttonBoardActions() {
 		
 		app.getBtn00().addActionListener(new ActionListener() {
@@ -130,17 +146,9 @@ public class ControllerGame {
 		});	
 	}
 
-	/**
-	 * Method that updates the movement label with the movement that has been made
-	 * 
-	 * @param name the name of the player that has made the movement
-	 */
-	public void movementMade(String name) {
-		app.getLblMovements().setText(name+", coloca ficha ...");
-	}
 	
 	/**
-	 * Method that check if a movement can be made and if it can , makes it
+	 * Method that check if a movement can be made and if it can , makes it , also check if the player have won with the current movement
 	 * 
 	 * @param btn button that has been triggered 
 	 * @param column Column where the button is
@@ -157,9 +165,9 @@ public class ControllerGame {
 					if(ply1.getNumPlacedTokens() < 3 && tempBoard[column][row].equals("")) { // If the player haven't put 3 tokens in the board and the field selected is null
 						
 						btn.setText(ply1.getToken()); // The token is put in the board in the eyes of the user
-						tempBoard[column][row] = ply1.getToken(); // it's save the position in which it have been put it
+						tempBoard[column][row] = ply1.getToken(); // it's save the position in which it has been put it
 						
-						movementMade(this.ply1.getName()); // Update the movement label
+						app.getLblMovements().setText(ply1.getName()+", coloca ficha ..."); // Update the movement label
 						ply1.setNumPlacedTokens(this.ply1.getNumPlacedTokens()+1); // Add a token to the user token counter
 						
 						changeTurn = true; //Indicates that a token has been put
@@ -171,9 +179,8 @@ public class ControllerGame {
 						tempBoard[column][row] = ""; // Clear the position in which it had been put it
 
 			 
-						movementMade(this.ply1.getName()); // Update the movement label
 						ply1.setNumPlacedTokens(this.ply1.getNumPlacedTokens()-1); // Subtract 1 of the token counter
-						app.getLblMovements().setText(ply1.getName()+", coge una ficha ..."); //Indicates that a token have been subtracted
+						app.getLblMovements().setText(ply1.getName()+", coge una ficha ..."); //Indicates that a token has been subtracted
 					}
 					
 					
@@ -184,7 +191,7 @@ public class ControllerGame {
 						btn.setText(ply2.getToken());
 						tempBoard[column][row] = ply2.getToken();
 						
-						movementMade(this.ply2.getName());
+						app.getLblMovements().setText(ply2.getName()+", coloca ficha ...");
 						this.ply2.setNumPlacedTokens(this.ply2.getNumPlacedTokens()+1);
 						changeTurn = true;
 												
@@ -198,18 +205,21 @@ public class ControllerGame {
 				}
 				
 				board.setField(tempBoard); // Update the real board	
+				 
 				
-				
-				if(board.checkWin(turn)) { //Check if the current player have win the game 
+				if(ply1.getNumPlacedTokens() == 3 || ply2.getNumPlacedTokens() == 3) { //If some player have 3 tokens place in the board
 					
-					if(turn.equals("X")) {
-						JOptionPane.showMessageDialog(null, ply1.getName() +" ha ganado!");
-	
-					}else {
-						JOptionPane.showMessageDialog(null, ply2.getName() +" ha ganado!");					
+					if(board.checkWin(turn)) { //Check if the current player have won the game 
+						
+						if(turn.equals("X")) {
+							JOptionPane.showMessageDialog(null, ply1.getName() +" ha ganado!");
+		
+						}else {
+							JOptionPane.showMessageDialog(null, ply2.getName() +" ha ganado!");					
+						}
+						
+						gameStarted = false; 
 					}
-					
-					gameStarted = false; 
 				}
 				
 				if(changeTurn) {   //Change the turn of the player if a token have been made
